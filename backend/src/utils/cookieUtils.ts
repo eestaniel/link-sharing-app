b3764ac0d3eba2ae5ae9a53cf9d568ed465337cf
EEstaniel <edwineestaniel93@gmail.com>
2025-02-19 14:54:52 -0800

    refactor: change domain to env

--- backend/src/utils/cookieUtils.ts
+++ backend/src/utils/cookieUtils.ts
@@ -9,7 +9,7 @@ export const setCookie = (res: Response, name: string, value:string, options = {
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax', // strict for production, lax for development
     maxAge: 1000 * 60 * 60 * 24, // 1 week
-    domain: 'localhost',
+    domain: process.env.DOMAIN,
     ...options
   });
 }
