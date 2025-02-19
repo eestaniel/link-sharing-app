92dea9806ba8f71a8ecc466bc798214e9ee18070
EEstaniel <edwineestaniel93@gmail.com>
2025-02-19 14:41:53 -0800

    fix: bug

--- backend/express-augmentations.d.ts
+++ backend/express-augmentations.d.ts
@@ -1,12 +1,9 @@
-// src/express-augmentations.d.ts
-import { Request } from 'express';
-import { File } from 'multer';
+import 'multer'; // ensures Express.Multer is loaded
 
 declare global {
   namespace Express {
     interface Request {
-      file?: File;
-      files?: File[]; // If you also use upload arrays
+      file?: Express.Multer.File;    // correct approach
     }
   }
 }
