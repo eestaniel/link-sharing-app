3dde160ab4b7021a188ab444621d8bce8d7952af
EEstaniel <edwineestaniel93@gmail.com>
2025-02-20 11:10:04 -0800

    chore: change sameSite

--- backend/src/utils/cookieUtils.ts
+++ backend/src/utils/cookieUtils.ts
@@ -7,7 +7,7 @@ export const setCookie = (res: Response, name: string, value:string, options = {
   res.cookie(name, value, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax', // strict for production, lax for development
+    sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
     maxAge: 1000 * 60 * 60 * 24, // 1 week
     domain: process.env.DOMAIN,
     ...options
